87d4c0bf754bdf7f10f9ced1ca73b2002b2ab826
jeong214132@gmail.com <jeong214132@naver.com>
2021-04-26 16:37:23 +0900

    user register

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {User} = require("./models/User");
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://patrick:1203658a!@boiler-plate.ujirl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/api/users/register', (req, res) => {
+
+  //회원 가입 할떄 필요한 정보들을  client에서 가져오면 
+  //그것들을  데이터 베이스에 넣어준다. 
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
